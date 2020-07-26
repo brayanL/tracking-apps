6e6b4299b3569a80363ebe58357fa2c207f8af46
Brayan Loayza <godsarmybl@gmail.com>
2020-07-25 19:25:16 -0500

    Layout config and showToursByRangeDate service.

--- tracking-android/app/src/main/java/tracking/com/trackingandroid/apps/tours/ui/ToursAdapter.java
+++ tracking-android/app/src/main/java/tracking/com/trackingandroid/apps/tours/ui/ToursAdapter.java
@@ -20,7 +20,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
---
+++ tracking-web/src/apps/reports/components/ReportAdvanced.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ReportAdvanced() {
+    return (
+        <div>
+            <p>Whatever</p>
+        </div>
+    );
+}
