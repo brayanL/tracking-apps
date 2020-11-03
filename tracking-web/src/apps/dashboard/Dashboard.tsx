6184147bf55f34f2d4061f6bb756ed2ae8c40bb4
Brayan Loayza <godsarmybl@gmail.com>
2020-11-03 11:56:29 -0500

    Show better description dashboard.

--- tracking-web/src/apps/dashboard/Dashboard.tsx
+++ tracking-web/src/apps/dashboard/Dashboard.tsx
@@ -3,7 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
     BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart, Area,
 } from 'recharts';
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
+import * as moment from 'moment';
+import 'moment/locale/es';
 
 import { startDashboard } from '../common/actions';
 import { RootState } from '../../store/reducers';
@@ -12,11 +14,11 @@ export default function Dashboard() {
     const dispatch = useDispatch();
     const dashboard = useSelector((state: RootState) => state.reports.dashboard);
     const distanceBetweenReport = dashboard.distanceBetween
-        .map(({ distanceBetween, month }) => ({ name: month, distanceBetween }));
+        .map(({ distanceBetween, month }) => ({ name: moment.months(month), distanceBetween }));
     const timeTraveledReport = dashboard.timeTraveled
-        .map(({ timeTraveled, month }) => ({ name: month, timeTraveled }));
+        .map(({ timeTraveled, month }) => ({ name: moment.months(month), timeTraveled }));
     const tourPerMonthReport = dashboard.tourPerMonth
-        .map(({ totalTrips, month }) => ({ name: month, totalTrips }));
+        .map(({ totalTrips, month }) => ({ name: moment.months(month), totalTrips }));
 
     useEffect(() => {
         dispatch(startDashboard());
@@ -27,56 +29,78 @@ export default function Dashboard() {
             <Grid item xs={6}>
                 <Grid container direction="column">
                     <Grid item xs={6}>
-                        <BarChart
-                            width={600}
-                            height={310}
-                            data={distanceBetweenReport || []}
-                            margin={{
-                                top: 5, right: 30, left: 20, bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="distanceBetween" fill="#82ca9d" barSize={20} />
-                        </BarChart>
+                        <Grid container direction="column" spacing={2}>
+                            <Grid item>
+                                <Button variant="contained">Detalles</Button>
+                            </Grid>
+                            <Grid item>
+                                <BarChart
+                                    width={600}
+                                    height={310}
+                                    data={distanceBetweenReport || []}
+                                    margin={{
+                                        top: 5, right: 30, left: 20, bottom: 5,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="name" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Bar name="Distancia Recorrida" dataKey="distanceBetween" fill="#82ca9d" barSize={20} />
+                                </BarChart>
+                            </Grid>
+                        </Grid>
                     </Grid>
                     <Grid item xs={6}>
-                        <BarChart
-                            width={600}
-                            height={310}
-                            data={tourPerMonthReport || []}
-                            margin={{
-                                top: 5, right: 30, left: 20, bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="totalTrips" fill="#66ccff" barSize={20} />
-                        </BarChart>
+                        <Grid container direction="column" spacing={2}>
+                            <Grid item>
+                                <Button variant="contained">Detalles</Button>
+                            </Grid>
+                            <Grid item>
+                                <BarChart
+                                    width={600}
+                                    height={310}
+                                    data={tourPerMonthReport || []}
+                                    margin={{
+                                        top: 5, right: 30, left: 20, bottom: 5,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="name" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Bar name="Viajes por mes" dataKey="totalTrips" fill="#66ccff" barSize={20} />
+                                </BarChart>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
             <Grid container item xs={6} alignItems="center">
-                <AreaChart
-                    width={500}
-                    height={400}
-                    data={timeTraveledReport}
-                    margin={{
-                        top: 10, right: 30, left: 0, bottom: 0,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey="timeTraveled" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item container justify="flex-end">
+                        <Button variant="contained">Detalles</Button>
+                    </Grid>
+                    <Grid item>
+                        <AreaChart
+                            width={500}
+                            height={400}
+                            data={timeTraveledReport}
+                            margin={{
+                                top: 10, right: 30, left: 0, bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Area name="Tiempo viajado" type="monotone" dataKey="timeTraveled" stroke="#8884d8" fill="#8884d8" />
+                        </AreaChart>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
--- tracking-web/src/utils/CommonUtils.ts
+++ tracking-web/src/utils/CommonUtils.ts
@@ -18,3 +18,16 @@ export const handleSaveToPC = (jsonData) => {
     link.href = url;
     link.click();
 };
+
+export function msToTime(duration: number) {
+    const milliseconds = parseInt(String((duration % 1000) / 100));
+    let seconds: string | number = Math.floor((duration / 1000) % 60);
+    let minutes: string | number = Math.floor((duration / (1000 * 60)) % 60);
+    let hours: string | number = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? `0${hours}` : hours;
+    minutes = (minutes < 10) ? `0${minutes}` : minutes;
+    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
